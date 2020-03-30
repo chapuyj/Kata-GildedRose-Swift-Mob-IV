de07b69e3a514dc78b973ef6e2fd0b8727b7b4e2
Jordan Chapuy <chapuyj@gmail.com>
2020-03-30 11:06:58 +0200

    Mob-Programming #1 - Starting and reading specs and code. Exploration for unit testing. Discussions around different ways to do it.

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -3,16 +3,60 @@ import XCTest
 
 class GildedRoseTests: XCTestCase {
 
-    func testFoo() {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+    func testAgedBrie_SellIn2_AfterOneDay_QualityShouldIncreaseByOne_SellInShouldDecreaseByOne() {
+        let items = [Item(name: "Aged Brie", sellIn: 2, quality: 0)]
         let app = GildedRose(items: items);
+
         app.updateQuality();
-        XCTAssertEqual("fixme", app.items[0].name);
+
+        XCTAssertEqual(1, app.items[0].sellIn);
+        XCTAssertEqual(1, app.items[0].quality);
+    }
+
+    func testAgedBrie_SellInNegative_AfterOneDay_QualityShouldIncreaseByTwo_SellInShouldDecreaseByOne() {
+        let items = [Item(name: "Aged Brie", sellIn: -2, quality: 0)]
+        let app = GildedRose(items: items);
+
+        app.updateQuality();
+
+        XCTAssertEqual(-3, app.items[0].sellIn);
+        XCTAssertEqual(2, app.items[0].quality);
+    }
+
+    func testUpdateQuality_SellIn20() {
+        let items = [
+            Item(name: "+5 Dexterity Vest", sellIn: 20, quality: 20),
+            Item(name: "Aged Brie", sellIn: 20, quality: 20),
+            Item(name: "Elixir of the Mongoose", sellIn: 20, quality: 20),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 20, quality: 20),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 20, quality: 20)
+        ]
+        let app = GildedRose(items: items);
+
+        app.updateQuality();
+
+        let expectedItemsAfterOneDay = [
+            Item(name: "+5 Dexterity Vest", sellIn: 19, quality: 19),
+            Item(name: "Aged Brie", sellIn: 19, quality: 21),
+            Item(name: "Elixir of the Mongoose", sellIn: 19, quality: 19),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 20, quality: 20),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 19, quality: 21)
+        ]
+
+        XCTAssertEqual(expectedItemsAfterOneDay, app.items)
     }
 
     static var allTests : [(String, (GildedRoseTests) -> () throws -> Void)] {
         return [
-            ("testFoo", testFoo),
+            ("testAgedBrie_SellIn2_AfterOneDay_QualityShouldIncreaseByOne_SellInShouldDecreaseByOne", testAgedBrie_SellIn2_AfterOneDay_QualityShouldIncreaseByOne_SellInShouldDecreaseByOne),
+            ("testAgedBrie_SellInNegative_AfterOneDay_QualityShouldIncreaseByTwo_SellInShouldDecreaseByOne", testAgedBrie_SellInNegative_AfterOneDay_QualityShouldIncreaseByTwo_SellInShouldDecreaseByOne),
+            ("testUpdateQuality_SellIn20", testUpdateQuality_SellIn20)
         ]
+    }
+}
+
+extension Item: Equatable {
+    public static func == (lhs: Item, rhs: Item) -> Bool {
+        return lhs.description == rhs.description
     }
 }
